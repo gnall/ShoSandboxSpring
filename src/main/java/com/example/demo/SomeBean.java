61d3a9ed04d2a3830fb8f9cbbe59116853e64ae2
josephgnall <joseph.gnall@scranton.edu>
2017-07-06 18:48:41 -0400

    Initial

---
+++ src/main/java/com/example/demo/SomeBean.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import java.time.LocalDate;
+
+public class SomeBean {
+
+	private LocalDate datePlanted;
+
+	public LocalDate getDatePlanted() {
+		return datePlanted;
+	}
+
+	public void setDatePlanted(LocalDate datePlanted) {
+		this.datePlanted = datePlanted;
+	}
+
+	// include getter and setter
+
+}
